6254e44209e9dbbb8d220e218850fa9b087dc2f6
1103-Anthony-Lyster <alyster@nevada.unr.edu>
2024-05-02 22:16:49 +0000

    updated files and created c file for project

---
+++ images.c
@@ -0,0 +1,24 @@
+//Authors: Anthony Lyster & Kevin Valadez
+//Date: 05/07/2024
+//Purpose: Final project, group coding assignment
+
+#include <stdio.h>
+
+int main(){
+
+	return 0;
+}
+
+void editImage(){
+
+}
+void cropImage(){
+
+}
+void dimImage(){
+
+}
+void brightenImage(){
+
+}
+
